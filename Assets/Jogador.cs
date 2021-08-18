068e69519c998b2a24e18de65a0751d5558f61e0
Victor Pereira <vpereirabr@gmail.com>
2021-08-18 09:19:30 +0200

    Initial commit

---
+++ Assets/Jogador.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jogador : MonoBehaviour
+{
+
+    private int _totalPontos = 0;
+    private int _vidas = 3;
+
+    private bool facingRight = true;
+
+    private bool isGrounded = true;
+
+    public float jumpHeight = 6.5f;
+    public float maxSpeed = 3f;
+
+    private float moveDirection = 0f;
+
+    public Rigidbody2D rb;
+
+
+    public int Vidas
+    {
+        get { return _vidas; }
+    }
+
+    public int Pontos
+    {
+        get { return _totalPontos; }
+    }
+
+    public void TiraVida()
+    {
+        if (_vidas > 0)
+            _vidas -= 1;
+    }
+
+    public void AddPontos()
+    {
+        _totalPontos += 10;
+    }
+
+    private void Update()
+    {
+        // Movement controls
+        if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) && (isGrounded || Mathf.Abs(rb.velocity.x) > 0.01f))
+        {
+            moveDirection = Input.GetKey(KeyCode.A) ? -1 : 1;
+        }
+        else
+        {
+            if (isGrounded || rb.velocity.magnitude < 0.01f)
+            {
+                moveDirection = 0;
+            }
+        }
+
+        // Change facing direction
+        if (moveDirection != 0)
+        {
+            if (moveDirection > 0 && !facingRight)
+            {
+                facingRight = true;
+                transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            }
+            if (moveDirection < 0 && facingRight)
+            {
+                facingRight = false;
+                transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            }
+        }
+
+        // Jumping
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2((moveDirection) * maxSpeed, rb.velocity.y);
+    }
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+}
