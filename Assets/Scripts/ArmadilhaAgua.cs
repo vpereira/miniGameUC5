d56c80d897d724976b1d8a2f44670644bac032fc
Victor Pereira <vpereirabr@gmail.com>
2021-08-22 19:12:48 +0200

    Add Armadilha agua

---
+++ Assets/Scripts/ArmadilhaAgua.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmadilhaAgua : MonoBehaviour
+{
+    private Jogador jogador;
+
+    private bool collided = false;
+
+    private void Awake()
+    {
+        jogador = GameObject.FindWithTag("Player").GetComponent<Jogador>();
+    }
+
+    IEnumerator OnTriggerEnter2D(Collider2D collision)
+    {
+        // TODO do we need to check if we are touching a player?
+        collided = true;
+
+        while(collided)
+        {
+            yield return new WaitForSeconds(1);
+
+            if (collided == true)
+            {
+                Debug.Log("TiraVida");
+                jogador.TiraVida(1);
+            }
+        }
+
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        collided = false;
+    }
+}
