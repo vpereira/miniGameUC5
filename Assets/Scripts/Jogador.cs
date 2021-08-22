b7a83658eacaeef323e42b362803f815ff177042
Victor Pereira <vpereirabr@gmail.com>
2021-08-22 08:17:07 +0200

    Add PhysicMaterials to the Player

--- Assets/Scripts/Jogador.cs
+++ Assets/Scripts/Jogador.cs
@@ -19,7 +19,7 @@ public class Jogador : MonoBehaviour
 
     private float moveDirection = 0f;
 
-    public Rigidbody2D rb;
+    private Rigidbody2D rb;
 
 
     public int Vidas
@@ -79,7 +79,8 @@ public class Jogador : MonoBehaviour
         // Jumping
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+            // rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+            rb.velocity = Vector2.up * jumpHeight;
         }
     }
 
