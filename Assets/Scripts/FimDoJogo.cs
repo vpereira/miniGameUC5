7d8706f7a93554c5596151205b92467639e0deff
Victor Pereira <vpereirabr@gmail.com>
2021-09-01 20:05:54 +0200

    Adicionada condicao de vitoria

---
+++ Assets/Scripts/FimDoJogo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FimDoJogo : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI finalMessage;
+
+    void Start()
+    {
+        finalMessage.enabled = false;        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            finalMessage.enabled = true;
+        }
+    }
+}
