fb1c9c8433fc92f6de2f5c699d566128bbb25418
Victor Pereira <vpereirabr@gmail.com>
2021-09-01 19:55:03 +0200

    Add score text

---
+++ Assets/Scripts/Pontos.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Pontos : MonoBehaviour
+{
+    [SerializeField]
+    private Jogador jogador;
+
+    [SerializeField]
+    private TextMeshProUGUI textScore;
+
+    // Update is called once per frame
+    void Update()
+    {
+        textScore.text = $"Pontos: {jogador.Pontos} ";
+    }
+}
