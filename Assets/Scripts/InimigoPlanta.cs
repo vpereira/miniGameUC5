e5f6e51b01649f9a5d80c22ba2b8ba3efbaa8667
Victor Pereira <vpereirabr@gmail.com>
2021-09-05 09:27:03 +0200

    Planta atira projeteis

---
+++ Assets/Scripts/InimigoPlanta.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InimigoPlanta : MonoBehaviour
+{
+    public GameObject projetil;
+
+    bool inimigoOnRange = false;
+
+    IEnumerator OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+            inimigoOnRange = true;
+
+        while(inimigoOnRange)
+        {
+            yield return new WaitForSeconds(1);
+
+            if (inimigoOnRange)
+            {
+                var startPosition = transform.position;
+                Instantiate(projetil, startPosition, Quaternion.identity);
+            }
+        }
+
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+            inimigoOnRange = false;
+    }
+}
---
+++ Assets/Scripts/Projetil.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projetil : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    private Jogador jogador;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        jogador = GameObject.FindWithTag("Player").GetComponent<Jogador>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = transform.right * 5f;  
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player")) {
+            jogador.TiraVida(1);
+            Destroy(gameObject);
+        }
+    }
+}
